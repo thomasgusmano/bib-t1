5cc8c7cd89bb0ac6339f8e22fb540040517ccea2
thomasgusmano <gusmanothomas1@gmail.com>
2025-07-04 01:06:19 +0200

    Add files via upload

---
+++ libreria/db_chat.php
@@ -0,0 +1,12 @@
+<?php 
+$host = "localhost"; 
+$user = "root";
+$pass = ""; 
+$db_name = "biblio_t1"; 
+
+$con = new mysqli($host,$user,$pass,$db_name);
+
+function formatDate($date){
+	return date('g:i a', strtotime($date));
+}
+?>
